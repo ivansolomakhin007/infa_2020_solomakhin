2112bfa98f8cdb7153f5ca2217b00d2b6323c36c
Ivan <svan5003@gmail.com>
2020-10-30 10:36:01 +0300

    refucktored

--- lab9/dragon_curve.py
+++ lab9/dragon_curve.py
@@ -7,26 +7,47 @@ turtle.tracer(False)
 # создаем грамматику
 circuit = "FX"
 rules = {"X": "X+YF+",
-         "Y": "−FX−Y"}
+         "Y": "−FX−Y",
+         "F": "F",
+         "−": "−",
+         "+": "+"}
+
+
 def next_generation(c):
     c = list(c)
     for i in range(len(c)):
-        if c[i] == "X":
-            c[i] = "X+YF+"
-        elif c[i] == "Y":
-            c[i] = "−FX−Y"
+        c[i] = rules[c[i]]
     return str("".join(c))
 
 
 for i in range(14):
     circuit = next_generation(circuit)
+    print(i)
+
+
+def forward():
+    turtle.forward(10)
+
+
+def right():
+    turtle.right(90)
+
+
+def left():
+    turtle.left(90)
+
+
+def nothing():
+    pass
+
+
+moves = {"F": forward,
+         "+": right,
+         "−": left,
+         "X": nothing,
+         "Y": nothing}
 
 for elem in circuit:
-    if elem == "F":
-        turtle.forward(10)
-    elif elem == "+":
-        turtle.right(90)
-    elif elem == "−":
-        turtle.left(90)
+    moves[elem]()
 
 turtle.exitonclick()
